ec7a595bdc92e967fd10f65f393ec1c6f2f3dcb1
Luis Perez <Luis998.ap@gmail.com>
2026-05-13 14:48:21 -0500

    Corrected redirect link on Checkaccess.js

--- src/components/CheckAccess.js
+++ src/components/CheckAccess.js
@@ -5,7 +5,7 @@ export function CheckAccess({children, allowedRoles}) {
     const { role, loading } = useRole();
     console.log(role)
     if (loading) return <div>Loading...</div>;
-    if (!role) return <Navigate to="/unauthorizedaccess"/>;
+    if (!role) return <Navigate to="/"/>;
     if (!allowedRoles.includes(role)) return <Navigate to="/unauthorizedaccess" />;
 
     return children;
