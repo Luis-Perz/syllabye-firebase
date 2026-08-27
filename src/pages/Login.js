00aa44eeb093a7fda173e2b21c7f7bbc121beb30
M-Paez <157328953+M-Paez@users.noreply.github.com>
2026-08-27 11:47:55 -0500

    Updating access + login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,9 +13,9 @@ function Login(){
         try {
             const result = await signInWithPopup(auth, provider)
             if (!result.user.email.endsWith('@lewisu.edu')) {
-                console.log("Access denied. Please sign in with your Lewis email.");
+                console.log("Access denied. Please sign in with your Google email.");
                 await auth.signOut();
-                setError("Access denied. Please sign in with your Lewis email.");
+                setError("Access denied. Please sign in with your Google email.");
                 return;
             }
             console.log("success")
